1b3364357ab3d8983b636c813971936fa1fc0371
Gaurav Gurjar <ggurjar333@gmail.com>
2024-08-18 13:38:27 +0000

    updates README.md

--- backend-container/historical-data-apis/src/controllers/dataController.js
+++ backend-container/historical-data-apis/src/controllers/dataController.js
@@ -3,6 +3,45 @@ const dataService = require('../services/dataService');
 const { ClientSecretCredential } = require('@azure/identity');
 const { BlobServiceClient } = require('@azure/storage-blob');
 
+
+// Azure AD credentials
+const tenantId = "364661e2-3ab8-4544-a3ae-fc0b5adb4aaf";
+const clientId = "d9e5bd31-3513-48e3-a767-6ef1d485999a";
+const clientSecret = "d83d70d0-7e79-4c88-add6-41abe3ce2526";
+
+// Storage account details
+const accountName = "nflake";
+const containerName = "loff-datalake-nfl-bronze-dev";
+
+const credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+const blobServiceClient = new BlobServiceClient(`https://${accountName}.blob.core.windows.net`, credential);
+
+exports.getBlobData = async (req, res) => {
+    const blobName = req.params.blobName;
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+    try {
+        const downloadResponse = await blockBlobClient.download();
+        const blobData = await streamToString(downloadResponse.readableStreamBody);
+        res.send(blobData);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+async function streamToString(readableStream) {
+    return new Promise((resolve, reject) => {
+        const chunks = [];
+        readableStream.on("data", (data) => {
+            chunks.push(data.toString());
+        });
+        readableStream.on("end", () => {
+            resolve(chunks.join(""));
+        });
+        readableStream.on("error", reject);
+    });
+}
 exports.createData = (req, res) => {
   const data = req.body;
   const result = dataService.saveData(data);
